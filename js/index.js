1229e633ce8286b43c0f51cf2ae058b2923a2e5d
Guilherme Benjamin Sordi <estagiario04.ti@chiaperini.com.br>
2023-06-22 13:59:13 -0300

    update. formatter the code structures

--- js/index.js
+++ js/index.js
@@ -1,56 +1,29 @@
-const hamMenuBtn = document.querySelector(
-    ".header__main-ham-menu-cont"
-  ),
-  smallMenu = document.querySelector(
-    ".header__sm-menu"
-  ),
-  headerHamMenuBtn = document.querySelector(
-    ".header__main-ham-menu"
-  ),
+const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont"),
+  smallMenu = document.querySelector(".header__sm-menu"),
+  headerHamMenuBtn = document.querySelector(".header__main-ham-menu"),
   headerHamMenuCloseBtn = document.querySelector(
     ".header__main-ham-menu-close"
   ),
-  headerSmallMenuLinks = document.querySelectorAll(
-    ".header__sm-menu-link"
-  );
+  headerSmallMenuLinks = document.querySelectorAll(".header__sm-menu-link");
 
-hamMenuBtn.addEventListener(
-  "click", () => {
-    smallMenu.classList.contains(
-        "header__sm-menu--active"
-      ) ? smallMenu.classList.remove(
-        "header__sm-menu--active"
-      ) : smallMenu.classList.add(
-        "header__sm-menu--active"
-      ),
-      headerHamMenuBtn.classList.contains(
-        "d-none"
-      ) ? (headerHamMenuBtn.classList.remove(
-          "d-none"
-        ),
-        headerHamMenuCloseBtn.classList.add(
-          "d-none"
-        )
-      ) : (headerHamMenuBtn.classList.add(
-        "d-none"
-      ), headerHamMenuCloseBtn.classList.remove("d-none"))
-  }
-);
+hamMenuBtn.addEventListener("click", () => {
+  smallMenu.classList.contains("header__sm-menu--active")
+    ? smallMenu.classList.remove("header__sm-menu--active")
+    : smallMenu.classList.add("header__sm-menu--active"),
+    headerHamMenuBtn.classList.contains("d-none")
+      ? (headerHamMenuBtn.classList.remove("d-none"),
+        headerHamMenuCloseBtn.classList.add("d-none"))
+      : (headerHamMenuBtn.classList.add("d-none"),
+        headerHamMenuCloseBtn.classList.remove("d-none"));
+});
 
-for (
-  let e = 0; e < headerSmallMenuLinks.length; e++
-)
-
-  headerSmallMenuLinks[e].addEventListener(
-    "click",
-    () => {
-      smallMenu.classList.remove(
-          "header__sm-menu--active"
-        ),
-
-        headerHamMenuBtn.classList.remove("d-none"), headerHamMenuCloseBtn.classList.add("d-none")
-    });
+for (let e = 0; e < headerSmallMenuLinks.length; e++)
+  headerSmallMenuLinks[e].addEventListener("click", () => {
+    smallMenu.classList.remove("header__sm-menu--active"),
+      headerHamMenuBtn.classList.remove("d-none"),
+      headerHamMenuCloseBtn.classList.add("d-none");
+  });
 const headerLogoConatiner = document.querySelector(".header__logo-container");
 headerLogoConatiner.addEventListener("click", () => {
-  location.href = "index.html"
+  location.href = "index.html";
 });
